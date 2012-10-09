d841a1b704b4ca3dbc7fc26cbe5c57eef35cef66
CurlyMoo <root@pi.(none)>
2012-10-09 22:07:26 +0200

    Updates setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+apt-get install unzip
+
+wget https://github.com/as00270/xbian-1.0-fs-permissions-todo/zipball/master
+mv master xbian.zip
+unzip xbian.zip
+cd as00270-xbian-1.0-fs-permissions-todo-*
+mv -R etc/* /etc/
+mv -R usr/* /usr/
+
 #Setup init scripts
 update-rc.d resizesd start 2
 update-rc.d xbmc defaults
