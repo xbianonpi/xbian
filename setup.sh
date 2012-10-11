2c2618de9b8114a7f442cd039ff73cbe7ef03065
CurlyMoo <root@pi.(none)>
2012-10-11 21:46:55 +0200

    Creation xbian user, deletion pi user

--- setup.sh
+++ setup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+#11-10-2012 21:39
+#Setting up xbian user and deleting pi user
+sudo useradd -G sudo -m -s /bin/bash xbian
+sudo echo "xbian:raspberry" | chpasswd
+
+#Deleting user pi
+sudo userdel pi
+sudo rm -rf /home/pi
+
+#09-10-2012 22:00
+#Installing and dowloading git files
 apt-get install unzip
 
 wget https://github.com/as00270/xbian-1.0-fs-permissions-todo/zipball/master
@@ -9,6 +20,7 @@ cd as00270-xbian-1.0-fs-permissions-todo-*
 mv -R etc/* /etc/
 mv -R usr/* /usr/
 
+#09-10-2012 22:00
 #Setup init scripts
 update-rc.d resizesd start 2
 update-rc.d xbmc defaults
