31807d28f698f12b7d347c78c1669e88aa8001a5
CurlyMoo <root@pi.(none)>
2012-10-18 01:44:13 +0200

    Added recompiled lirc

---
+++ CompileLirc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd /usr/src
+sudo git clone --depth 1 https://github.com/raspberrypi/tools.git
+cd tools
+sudo git checkout 9c3d7b6ac692498dd36fec2872e0b55f910baac1
+sudo ln -s /usr/src/tools/arm-bcm2708/arm-bcm2708-linux-gnueabi/bin/arm-bcm2708-linux-gnueabi-gcc /usr/bin/arm-bcm2708-linux-gnueabi-gcc
+
+#Install lirc for the default configuration files
+sudo apt-get install autoconf automake libtool gcc help2man lirc
+#Remove lirc because it will be replaced by a custom compiled version
+sudo apt-get remove lirc
+
+cd /usr/src
+sudo git clone --depth 5 git://lirc.git.sourceforge.net/gitroot/lirc/lirc
+cd lirc
+sudo ./autogen.sh
+sudo patch -p1 < lirc_xbian.patch
+sudo ./configure --with-driver=all CROSS_COMPILE=/usr/bin/
+sudo make CROSS_COMPILE=/usr/bin/ LC_CTYPE=en_US.UTF-8
+sudo make install CROSS_COMPILE=/usr/bin/ LC_CTYPE=en_US.UTF-8
+
