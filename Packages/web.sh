124a93543446692cd275efcf30277858f7b2fe58
CurlyMoo <root@pi.(none)>
2012-10-22 22:21:23 +0200

    Experimental package system

---
+++ Packages/web.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Description: Installs Apache2 & PHP5
+# Packages: apache2-mpm-worker, apache2-mpm-prefork, apache2-mpm-event, apache2-mpm-itk, apache2-mpm-itk, apache2.2-common, libapache2-mod-php5, libapache2-mod-php5filter, php5-cgi, php5-fpm, php5-common
+#
