e9b0ba5d9041c3081b055213c8d36846770a50db
CurlyMoo <mauricehendriks1@gmail.com>
2012-11-04 22:47:36 +0100

    Update Docs/CompileKernel.sh

--- Docs/CompileKernel.sh
+++ Docs/CompileKernel.sh
@@ -24,8 +24,6 @@
 cd /usr/src
 sudo git clone --depth 5 https://github.com/raspberrypi/tools.git
 cd tools
-#Revert to the version used when building the kernel to ensure success
-sudo git checkout 9c3d7b6ac692498dd36fec2872e0b55f910baac1
 sudo ln -s /usr/src/tools/arm-bcm2708/arm-bcm2708-linux-gnueabi/bin/arm-bcm2708-linux-gnueabi-gcc /usr/bin/arm-bcm2708-linux-gnueabi-gcc
 
 #Download the official kernel from the raspberry pi github
@@ -33,8 +31,6 @@ sudo mkdir /opt/raspberry
 cd /opt/raspberry
 sudo git clone --depth 5 git://github.com/raspberrypi/linux.git
 cd linux
-#Revert tot the version used when building the kernel to ensure success
-sudo git checkout 10182a3bc434b27740f81c2b836a1af943060241
 
 #Download the used config from the XBian github
 sudo wget https://raw.github.com/Koenkk/xbian/master/Patches/kernel/.config
@@ -48,16 +44,12 @@ sudo patch -p1 < kernel.lirc_rpi-0.2.patch
 sudo kill -9 $(pgrep xbmc)
 
 #Make steps with custom commands not supported by the official config
-sudo make CROSS_COMPILE=/usr/bin/ CONFIG_LIRC_STAGING=y CONFIG_LIRC_RPI=m CONFIG_I2C_DEV=m
-sudo make modules CROSS_COMPILE=/usr/bin/ CONFIG_LIRC_STAGING=y CONFIG_LIRC_RPI=m CONFIG_I2C_DEV=m
-sudo make modules_install CROSS_COMPILE=/usr/bin/ CONFIG_LIRC_STAGING=y CONFIG_LIRC_RPI=m CONFIG_I2C_DEV=m INSTALL_MOD_PATH=/
+sudo make CROSS_COMPILE=/usr/bin/ CONFIG_LIRC_STAGING=y CONFIG_LIRC_RPI=m CONFIG_I2C_DEV=m CONFIG_LIRC_RP1=m
+sudo make modules CROSS_COMPILE=/usr/bin/ CONFIG_LIRC_STAGING=y CONFIG_LIRC_RPI=m CONFIG_I2C_DEV=m CONFIG_LIRC_RP1=m
+sudo make modules_install CROSS_COMPILE=/usr/bin/ CONFIG_LIRC_STAGING=y CONFIG_LIRC_RPI=m CONFIG_I2C_DEV=m CONFIG_LIRC_RP1=m INSTALL_MOD_PATH=/
 
 #Implement the new kernel
 cp arch/arm/boot/Image /boot/kernel.img
-
-#Build custom 8192co module
-#Follow this guide:
-#http://www.raspberrypi.org/phpBB3/viewtopic.php?f=53&t=11162
 
 #Reboot
 reboot
