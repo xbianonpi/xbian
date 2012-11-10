50a345b3254fa700e210cde3a1a22da135a29fe7
CurlyMo <root@pi.(none)>
2012-11-10 01:00:56 +0100

    Revert "Revert "xbian user autologin + keyboard input fix""
    This reverts commit 9eeae58c7fb7751ed2ec7021d257838918197442.

---
+++ etc/profile.d/hideoutput.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#Copyright 2012 CurlyMo <development@xbian.org>
+#
+#This file is part of Xbian - XBMC on the Raspberry Pi.
+#
+#Xbian is free software: you can redistribute it and/or modify it under the 
+#terms of the GNU General Public License as published by the Free Software 
+#Foundation, either version 3 of the License, or (at your option) any later 
+#version.
+#
+#Xbian is distributed in the hope that it will be useful, but WITHOUT ANY 
+#WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+#FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+#details.
+#
+#You should have received a copy of the GNU General Public License along 
+#with Xbian. If not, see <http://www.gnu.org/licenses/>
+#
+if [[ -z $SSH_CONNECTION && $(who am i | grep -wo "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | wc -l) -eq 0 &&  "$TERM" != "screen"  && ! -n "$TMUX"  ]]; then
+        clear
+	export PS1="\e[40;30m";
+	echo -e '\e[40;30m';
+fi
